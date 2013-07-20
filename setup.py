d8058bea067ed997388dcc0903077ab8a703943e
Kapil Thangavelu <kapilt@gmail.com>
2013-07-20 08:54:32 -0400

    incr revision

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Python client for juju-core websocket api.
 
 setup(
     name="jujuclient",
-    version="0.0.5",
+    version="0.0.6",
     description="A juju-core/gojuju simple synchronous python api client.",
     author="Kapil Thangavelu",
     author_email="kapil.foss@gmail.com",
